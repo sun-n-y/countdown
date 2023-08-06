aca9dd0a83fb0dedc6ee0c4195ab9f5a3f25cb10
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-05 23:13:19 -0400

    add countdown js calculate time

--- app.js
+++ app.js
@@ -33,7 +33,36 @@ const getYear = futureDate.getFullYear();
 const getDate = futureDate.getDate();
 const getDay = daysArray[futureDate.getDay()];
 const getMonth = monthsArray[futureDate.getMonth()];
-const getHours = futureDate.getHours();
-const getMinutes = futureDate.getMinutes();
+const getHour = futureDate.getHours();
+const getMinute = futureDate.getMinutes();
 
-giveaway.textContent = `Giveaway Ends On ${getDay}, ${getMonth} ${getDate} ${getYear} at ${getHours}:${getMinutes} am`;
+giveaway.textContent = `Giveaway Ends On ${getDay}, ${getMonth} ${getDate} ${getYear} at ${getHour}:${getMinute} am`;
+
+const futureTime = futureDate.getTime();
+
+const currentDate = new Date();
+const currentTime = currentDate.getTime();
+
+const t = futureTime - currentTime;
+
+const getTime = function () {
+  const days = 24 * 60 * 60 * 1000;
+  const hours = 60 * 60 * 1000;
+  const mins = 60 * 1000;
+  const secs = 1000;
+
+  const getDays = Math.floor(t / days);
+  const getHours = Math.floor((t % days) / hours);
+  const getMins = Math.floor((t % hours) / mins);
+  const getSecs = Math.floor((t % mins) / secs);
+
+  const values = [getDays, getHours, getMins, getSecs];
+
+  const spans = document.querySelectorAll('span');
+
+  spans.forEach(function (value, index) {
+    value.textContent = values[index];
+  });
+};
+
+getTime();
